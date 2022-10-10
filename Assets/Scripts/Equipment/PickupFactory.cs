8d534fd04529ebce6a91f898da7eb1046846ae68
LightChase <chelseajennifer.ca@gmail.com>
2022-10-09 20:18:28 -0700

    Random drop chances added

--- Assets/Scripts/Equipment/PickupFactory.cs
+++ Assets/Scripts/Equipment/PickupFactory.cs
@@ -6,8 +6,10 @@ using UnityEngine.Assertions;
 public class PickupFactory : MonoBehaviour {
 
     public GameObject[] weapons;
+    
 
     [SerializeField] GameObject pickup;
+    private float randomNumber;
 
     private void Start() {
 
@@ -19,18 +21,27 @@ public class PickupFactory : MonoBehaviour {
 
     public void CreatePickup(Vector2 position) {
         //Todo: less hard code
-        //Todo: Drop chances/randomness
         GameObject GO = Instantiate(pickup);
         pickup.transform.position = position;
         Pickup newPickup = GO.GetComponent<Pickup>();
 
         Assert.IsNotNull(newPickup);
 
-        int itemIndex = Random.Range(0, weapons.Length);
+        randomNumber = Random.Range(1,10);
+        if(randomNumber < 3) //30% chance
+        {
+            int itemIndex = Random.Range(0, weapons.Length);
+            GameObject item = Instantiate(weapons[itemIndex]);           
+            item.transform.position = position;
+            newPickup.SetItem(item);  
+        }
+        else if(randomNumber < 5) //50% chance
+        {
 
-        GameObject item = Instantiate(weapons[itemIndex]);
-        item.transform.position = position;
-
-        newPickup.SetItem(item);
+        }
+        else if(randomNumber < 8) // 80% chance
+        {
+        }
+        
     }
 }
