c952b03fe2949e50f8c9590e2997e9ccb9ddc0bf
Para <catiethehuman@gmail.com>
2022-10-12 22:27:06 -0700

    Pickup Bugfix

--- Assets/Scripts/Equipment/PickupFactory.cs
+++ Assets/Scripts/Equipment/PickupFactory.cs
@@ -8,7 +8,7 @@ public class PickupFactory : MonoBehaviour {
     public GameObject[] weapons;
     public GameObject[] subWeapons;
     public GameObject[] mods;
-    
+
 
     [SerializeField] GameObject pickup;
     private float randomNumber;
@@ -26,36 +26,45 @@ public class PickupFactory : MonoBehaviour {
         //Todo: less hard code
         //Todo: implement more item variations
         //Todo: Drop tables probably
-        GameObject GO = Instantiate(pickup, position, Quaternion.identity);
-        Pickup newPickup = GO.GetComponent<Pickup>();
 
-        Assert.IsNotNull(newPickup);
 
-        randomNumber = Random.Range(1,10);
-        if(randomNumber < 3) //40% chance
-        {
+        randomNumber = Random.Range(1, 10);
+        Debug.Log(randomNumber);
+        if (randomNumber < 3) { //30% chance
+            Pickup newPickup = CreatePickupObject(position);
+
             int itemIndex = Random.Range(0, weapons.Length);
             GameObject item = Instantiate(weapons[itemIndex], position, Quaternion.identity);
             IEquipment newEquip = item.GetComponent<IEquipment>();
             Assert.IsNotNull(newEquip);
-            newPickup.SetItem(newEquip);  
-        }
-        else if(randomNumber < 5) //20% chance
-        {
-            //SubWeapons
-            int itemIndex = Random.Range(0, subWeapons.Length);
-            GameObject item = Instantiate(subWeapons[itemIndex], position, Quaternion.identity);   
-            IEquipment newEquip = item.GetComponent<IEquipment>();
             newPickup.SetItem(newEquip);
         }
-        else if(randomNumber < 8) // 15% chance
-        {
+        else if (randomNumber < 5) { //20% chance
+
+            Pickup newPickup = CreatePickupObject(position);
+            //SubWeapons
+            int itemIndex = Random.Range(0, subWeapons.Length);
+            GameObject item = Instantiate(subWeapons[itemIndex], position, Quaternion.identity);
+            IEquipment newEquip = item.GetComponent<IEquipment>();
+            Assert.IsNotNull(newEquip);
+            newPickup.SetItem(newEquip);
+        }
+        else if (randomNumber < 8) { //40% chance
+
             //Mods
             // int itemIndex = Random.Range(0, mods.Length);
             // GameObject item = Instantiate(mods[itemIndex]);           
             // item.transform.position = position;
             // newPickup.SetItem(item);  
         }
-        
+
+    }
+
+    private Pickup CreatePickupObject(Vector2 position) {
+        GameObject GO = Instantiate(pickup, position, Quaternion.identity);
+        Pickup newPickup = GO.GetComponent<Pickup>();
+
+        Assert.IsNotNull(newPickup);
+        return newPickup;
     }
 }
